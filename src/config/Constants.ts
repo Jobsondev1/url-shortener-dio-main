8f44f725d121d52a2f1c89bf14fc80c1f587f22e
jobsondev1 <jammeloalves@gmail.com>
2021-11-28 20:15:22 -0300

    first commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://jobson:35182982@cluster0.nnydh.mongodb.net/url-shortener-dio?retryWrites=true&w=majority+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+
+		}
